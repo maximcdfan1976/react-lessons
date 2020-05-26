7e2351b63d9aec1750d8e4aac8b48d26c388b5f2
Maxim <maximmikhaylov20101976@gmail.com>
2020-05-26 17:25:43 +0300

    Added4

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
 
             <div className="container">
 
-                <Aside/> {/*Aside3*/}
+                <Aside/> {/*Aside323*/}
 
              <Content/> {/*Container*/}
 
